2e9d9579b558b1b74dd12acb395562fc10eaf75d
Mostak Ahammed <mostakahammedofficial@gmail.com>
2022-05-30 18:48:36 +0600

    UWP-project

---
+++ BookSellerApp/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using BookSellerApp.Model;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace BookSellerApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        BookCollection books;
+        public MainPage()
+        {
+            this.InitializeComponent();
+            books = new BookCollection();
+            this.listDevices.ItemsSource = books.Get();
+        }
+
+        private void listDevices_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Book books = listDevices.SelectedItem as Book;
+            this.Frame.Navigate(typeof(Details), new NavParam { Data = books.Id });
+        }
+    }
+}
