60f5b5a855a668bcd8a73770b91599257b0a0a12
Mostak Ahammed <maruf.rbm@gmail.com>
2022-05-31 16:53:01 +0600

    Revert "Rem"
    This reverts commit 8cbeffaf6cecec71852304506d9199a198cf1aae.

---
+++ BookSellerApp/Details.xaml.cs
@@ -0,0 +1,41 @@
+﻿using BookSellerApp.Model;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace BookSellerApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Details : Page
+    {
+        public Details()
+        {
+            this.InitializeComponent();
+        }
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            var p = e.Parameter as NavParam;
+            BookCollection devices = new BookCollection();
+            this.DataContext = devices.Get(p.Data);
+        }
+        private void back_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(MainPage));
+        }
+    }
+}
